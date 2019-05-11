ca5d298b465cedbe262912e788ebcabf9504b51b
abeer-faizan <abeerofaizan@gmail.com>
2019-05-11 05:05:55 -0700

    First Commit

---
+++ marksheet.py
@@ -0,0 +1,76 @@
+sindhi = int(input('Mark Obtain in Sindhi: '))
+english = int(input('Mark Obtain in English: '))
+pakstd = int(input('Mark Obtain in Pakistan Studies: '))
+biologyTh = int(input('Mark Obtain in Biology Theory: '))
+biologyPr = int(input('Mark Obtain in Biology Practical: '))
+chemistryTh = int(input('Mark Obtain in Chemistry Theory: '))
+chemistryPr = int(input('Mark Obtain in Chemistry Practical: '))
+grade =''
+
+print('\n\t\t\t\tSTATEMENT OF MARKS')
+
+print('SUBJECT\t\t\tMAX MARKS\t MINPASS MARKS\t MARKS OBTAIN\t STATUS')
+if sindhi >= 25:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('SINDHI\t\t\t  075\t\t      025\t\t '+str(sindhi)+'\t  '+status)
+if english >= 25:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('ENGLISH\t\t\t  075\t\t      025\t\t '+str(english)+'\t  '+status)
+if pakstd >= 25:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('PAKISTAN STUDIES\t  075\t\t      025\t\t '+str(pakstd)+'\t  '+status)
+if biologyTh >= 28:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('BIOLOGY THEROY\t\t  085\t\t      028\t\t '+str(biologyTh)+'\t  '+status)
+if biologyPr >= 5:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('BIOLOGY PRACTICAL\t  015\t\t      005\t\t '+str(biologyPr)+'\t  '+status)
+if chemistryTh >= 28:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('CHEMISTRY THEORY\t  085\t\t      025\t\t '+str(chemistryTh)+'\t  '+status)
+
+if chemistryPr >= 5:
+    status = 'PASS'
+else:
+    status = 'FAIL'
+    grade = 'F'
+print('CHEMISTRY PRACTICAL\t  015\t\t      005\t\t '+str(chemistryPr)+'\t  '+status)
+
+totalMarksObt = sindhi + english + pakstd + biologyPr + biologyTh + chemistryPr + chemistryTh
+percentageTotal = totalMarksObt / 425 * 100
+
+if grade != 'F':
+    if percentageTotal >= 80:
+        grade = 'A+'
+    elif (percentageTotal < 80 and percentageTotal >= 70):
+        grade = 'A'
+    elif (percentageTotal < 70 and percentageTotal >= 60):
+        grade = 'B'
+    elif (percentageTotal < 60 and percentageTotal >= 50):
+        grade = 'C'
+    elif (percentageTotal < 50 and percentageTotal >= 40):
+        grade = 'D'
+    elif (percentageTotal > 40):
+        grade = 'E'
+
+print('\n\nTOTAL MARKS OBTAIN   '+str(totalMarksObt)+'   OUT OF 425')
+print('PERCENTAGE: '+str(round(percentageTotal,2))+'%')
+print('GRADE: '+grade+'\n')
