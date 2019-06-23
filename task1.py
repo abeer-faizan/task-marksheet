4e2b07ee33ef540014d14b0841b65a30fbf4cdac
abeer-faizan <50220359+abeer-faizan@users.noreply.github.com>
2019-06-23 22:15:38 +0500

    Delete task1.py

--- task1.py
+++
@@ -1,5 +0,0 @@
-import math
-print('---- Radius of Circle ----')
-radius = int(input('Enter the radiuse of circle: '))
-area = math.pi * math.pow(radius, 2)
-print('Area of the circle:', format(area, '.2f'))
